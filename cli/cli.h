d107418756f6089c10d15b0c5c03e6f66913ad99
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-10-22 13:03:42 -0300

    Create cli.h

---
+++ cli/cli.h
@@ -0,0 +1,64 @@
+#ifndef __CLI_H__
+#define __CLI_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "../utils/cli/utils-cli.h"
+#include "../utils/messages/messages.h"
+#include "../afd/afd.h"
+
+typedef enum operations
+{
+    VISUALIZATION = 1,
+    COMPLEMENT = 2,
+    RECOGNITION = 3,
+    INTERSSECTION = 1,
+    UNION = 2,
+    MINIMIZATION = 3,
+
+} Operations;
+
+typedef enum readSteps
+{
+    READ_QUANT_STATES = 1,
+    READ_STATES = 2,
+    READ_QUANT_SYMBOLS = 3,
+    READ_SYMBOLS = 4,
+    READ_QUANT_TRANSITIONS = 5,
+    READ_TRANSITIONS = 6,
+    READ_INITIAL_STATE = 7,
+    READ_QUANT_FINAL_STATES = 8,
+    READ_FINAL_STATES = 9,
+
+} ReadSteps;
+
+typedef struct _CLIErrors
+{
+    char *text;
+
+} CliErrors;
+
+typedef struct _CLI
+{
+    Operations *operation;
+    char *inputfile;
+    char *secondfile;
+    char *output;
+    signed short int volatile hasError;
+    CliErrors errors;
+
+} CLI;
+
+CLI *createCLI(int argc, char *argv[]);
+
+Operations getOperation(char *command);
+
+Operations getOperationByText(char *command);
+
+CliErrors *isValidInputs(int argc, char *argv[]);
+
+AFD *readFile(CLI *cli, char *filename);
+
+void *writeFile(AFD *afd, FILE *file);
+
+#endif
