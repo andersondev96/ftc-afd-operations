98207c38313c392e8950c08a647e0af6c6c4a517
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-10-22 13:03:34 -0300

    Create message.h

---
+++ utils/messages/messages.h
@@ -0,0 +1,11 @@
+#ifndef __MESSAGES_H__
+#define __MESSAGES_H__
+
+#define INVALID_PREFIX_OPERATION "A operação deve iniciar com o prefixo  \"--\" ";
+#define INVALID_OPERATION "Esta não é uma operação válida";
+#define INVALID_ARGUMENTS_QUANTITY "Quantidade inválida de argumentos";
+#define NOT_PROVIDER_A_FILE "Informe um arquivo";
+#define NOT_PROVIDER_A_OUTPUT_FILE_FLAG "Sinalize um arquivo de saída";
+#define FILE_NOT_FOUND "Arquivo não encontrado";
+
+#endif
