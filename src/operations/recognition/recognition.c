5edfd75feb0d0e4877a1f8c3a7708e506d8762fc
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-10-24 13:16:25 -0300

    Whrite recognition function

--- src/operations/recognition/recognition.c
+++ src/operations/recognition/recognition.c
@@ -3,11 +3,102 @@
 #include <stdlib.h>
 #include "./recognition.h"
 
-void recognitionAFD(AFD *afd, FILE *entry)
-{
-}
-
 void recognitionEntryPoint(CLI *cli, AFD *afd, int argc, char *argv[])
 {
-    printf("Reconhecimento");
+    // Verifica se há uma quantidade válida de argumentos
+    if (argc != 5)
+    {
+        printf("Quantidade de argumentos inválida, tente novamente!\n");
+        return;
+    }
+
+    // Verifica se foi passado um arquivo
+    if (argv[2] == NULL || argv[4] == NULL)
+    {
+        printf("Por favor, informe um arquivo!\n");
+        return;
+    }
+
+    // Verifica se foi informado um arquivo de saída
+    if (strcmp("--output", argv[3]) != 0)
+    {
+        printf("Por favor, informe um arquivo para saída!\n");
+        return;
+    }
+
+    cli->secondfile = argv[4];
+
+    // Abre o arquivo
+    FILE *file = fopen(cli->secondfile, "a");
+
+    // Verifica se o arquivo aberto não é NULL
+    if (file == NULL)
+    {
+        printf("Por favor, informe um arquivo!\n");
+        return;
+    }
+}
+
+char **readWords(char *fileName, int *size_ptr)
+{
+    FILE *file = fopen(fileName, "r");
+
+    if (!file)
+    {
+        return NULL;
+    }
+
+    int size = 0;
+    int **words = NULL;
+    char buffer[100];
+
+    while (fscanf(file, "%s\n", buffer) != EOF)
+    {
+        char *word = copyString(buffer);
+        size++;
+        if (words)
+        {
+            words = realloc(words, sizeof(char *) * size);
+        }
+        else
+        {
+            words = malloc(sizeof(char *));
+        }
+        words[size - 1] = word;
+    }
+
+    *size_ptr = size;
+    fclose(file);
+    return words;
+}
+
+void freeWords(char **words, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        free(words[i]);
+    }
+
+    free(words);
+}
+
+void writeRecognitionResult(int *result, char *fileName, int size)
+{
+    FILE *file = fopen(fileName, "w");
+    if (!file)
+    {
+        return;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(file, "%d\n", result[i]);
+    }
+}
+
+char *copyString(char *src)
+{
+    char *newString = malloc(sizeof(char) * strlen(src) + 1);
+    strcpy(newString, src);
+    return newString;
 }
--- src/operations/recognition/recognition.h
+++ src/operations/recognition/recognition.h
@@ -2,6 +2,14 @@
 #include "../../afd/afd.h"
 #include "../../cli/cli.h"
 
-void recognitionAFD(AFD *afd, FILE *entry);
-
 void recognitionEntryPoint(CLI *cli, AFD *afd, int argc, char *argv[]);
+
+char **readWords(char *fileName, int *size_ptr);
+
+void freeWords(char **words, int size);
+
+void writeRecognitionResult(int *result, char *fileName, int size);
+
+char *transitionFunction(AFD *afd, char *symbol, char *current_state);
+
+char *copyString(char *src);
